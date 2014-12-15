5ba8dd1fa3cf443d0a7750635a132f92b94e1904
Dan Gopstein <dan@gopstein.com>
2014-12-15 01:17:39 -0500

    Shorter code

--- nearest_half.rb
+++ nearest_half.rb
@@ -1,2 +1,2 @@
-(5..50).map{|x| x*0.5}.flat_map{|x| [0.10,0.15,0.20,0.25].map{|t| [x,((t*x)*2).round / 2.0]}}.map{|x, y| (10000*y/x).to_i/100.0}.uniq.sort.take(9)
+2.5.step(25, 0.5).flat_map{|x| [10,15,20,25].map{|t| [x,(2*t/100.0*x).round / 2.0]}}.map{|x, t| (10000*t/x).to_i/100.0}.uniq.sort.take(9)
 
